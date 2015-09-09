1b48353f06055fb9c8fbdb66d9e2e1da6d84c374
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-09-09 23:27:20 +0200

    Fix tests

--- tests/specs/MiddlewareSpec.php
+++ tests/specs/MiddlewareSpec.php
@@ -1,8 +1,12 @@
 <?php
 namespace League\Route\Stack;
 
+use League\Route\Dispatcher;
 use League\Route\RouteCollection;
 use PhpSpec\ObjectBehavior;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UriInterface;
 
 class MiddlewareSpec extends ObjectBehavior
 {
@@ -10,5 +14,23 @@ class MiddlewareSpec extends ObjectBehavior
     {
         $this->beConstructedWith($router);
         $this->shouldHaveType('League\Route\Stack\Middleware');
+    }
+
+    public function it_handles_request(
+        RouteCollection $router,
+        Dispatcher $dispatcher,
+        RequestInterface $request,
+        UriInterface $uri,
+        ResponseInterface $response)
+    {
+        $this->beConstructedWith($router);
+
+        $router->getDispatcher()->willReturn($dispatcher);
+        $dispatcher->dispatch('GET', '/path/to/endpoint')->willReturn($response);
+        $request->getUri()->willReturn($uri);
+        $uri->getPath()->willReturn('/path/to/endpoint');
+        $request->getMethod()->willReturn('GET');
+
+        $this->handle($request)->shouldReturn($response);
     }
 }
